6edab4fb3a4714cc618d831eeaa19eb15b8a3cc9
student <romanilichev@mail.ru>
2019-03-14 22:53:25 +0300

    перенос валидации на сервер

--- src/rest.js
+++ src/rest.js
@@ -36,8 +36,6 @@ server.get('/user', (req, res, next) => {
 });
 
 server.post('/auth', (req, res, next) => {
-    console.log('auth');
-
     let { username, password } = req.body;
     console.log(username, password);
     user.authenticate(username, password).then(data => {
@@ -68,34 +66,40 @@ server.post('/resultFlag', (req, res, next) => {
     console.log('Пришел объект:');
     console.log(req.body);
     let user = {username: req.body.nickname, password: req.body.password};
-    mongoClient.connect(function (err, client) {
-        const db = client.db("heroku_hw9cvg3q");
-        const collection = db.collection("users");
-        collection.find({username: req.body.nickname}).toArray(function (err, result) {
-            if (result.length === 0) {
-                resultFlag = 'true';
-                console.log(resultFlag);
-                console.log('Копий нет');
-                collection.insertOne(user, function (err, result) {
-                    console.log('Добавлено');
+    if (user.username.length > 4 && user.password.length > 7) {
+        mongoClient.connect(function (err, client) {
+            const db = client.db("heroku_hw9cvg3q");
+            const collection = db.collection("users");
+            collection.find({username: req.body.nickname}).toArray(function (err, result) {
+                if (result.length === 0) {
+                    resultFlag = 'true';
+                    console.log(resultFlag);
+                    console.log('Копий нет');
+                    collection.insertOne(user, function (err, result) {
+                        console.log('Добавлено');
+                        if (err) {
+                            return console.log(err);
+                        }
+                    });
+                    res.send(resultFlag);
+
+                } else {
+                    resultFlag = 'false';
+                    console.log(resultFlag);
+                    console.log('Есть копия, не добавлено');
+                    res.send(resultFlag);
+
                     if (err) {
                         return console.log(err);
                     }
-                });
-                res.send(resultFlag);
-
-            } else {
-                resultFlag = 'false';
-                console.log(resultFlag);
-                console.log('Есть копия, не добавлено');
-                res.send(resultFlag);
-
-                if (err) {
-                    return console.log(err);
                 }
-            }
+            });
         });
-    });
+    } else {
+        console.log('Не удовл. условиям');
+        resultFlag = 'Bad Request'; // отправит bad Request если нарушены условия по длине
+        res.send(resultFlag);
+    }
     next();
 });
 
